0f983e9505bdd30421ae57313cd1a60d64e54b32
LuzDev <140513070+Luzdeveloper@users.noreply.github.com>
2024-01-25 10:59:27 +0100

    Premier script

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,61 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+// Selections des éléments HTML
+
+const bannerImg = document.querySelector('.banner-img');
+const arrow_left = document.querySelector('.arrow_left');
+const arrow_right =document.querySelector('.arrow_right');
+const dots = document.querySelector('.dots');
+
+// Variable pour l'image actuelle du carroussel
+
+let numeroSlide = 0; 
+
+// Fonction pour mettre a jour le carroussel 
+
+function changerImage() {
+	bannerImg.src = slides[numeroSlide].image;
+	bannerImg.alt = slides[numeroSlide].tagLine;
+}
+
+//FOnction pour mettre a jour les dots
+
+function mouvementDots() {
+	dots.innerHTML = '';
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement('span');
+		// Ajout de la class 'active' au dot correspondant a l image active
+		if (i === numeroSlide) {
+			dot.className = 'dot active'
+		} else{
+			dot.className = dot;
+		}
+		dots.appendChild(dot) //appenChild sert a rajouté un noeud à la fin de liste d'enfant d'un noeud parent spécifié
+	}
+}
+
+//Fonction pour passer a l'image suivante 
+
+function imageSuivante() {
+	if (numeroSlide < slides.length - 1) {
+		numeroSlide++;
+	} else {
+		numeroSlide = 0;
+	}
+	changerImage()
+	mouvementDots()
+}
+
+
+//Ajout eventlistner pour les clics sur les fleches 
+
+arrow_left.addEventListener('click', imageSuivante);
+arrow_right.addEventListener('click', imageSuivante);
+
+//Reinitialisation des images et des dots 
+
+changerImage()
+mouvementDots()
